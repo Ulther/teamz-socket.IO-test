9ed7cc44ca080f0b21972b6f8ea03b88c481a472
schofield-r <yenschofield@gmail.com>
2019-12-18 11:01:08 +0000

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { subscribeToTimer } from "./Api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    timestamp: "no timestamp yet"
+  };
+  render() {
+    return <div>This is the timer value: {this.state.timestamp}</div>;
+  }
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) =>
+      this.setState({
+        timestamp
+      })
+    );
+  }
 }
 
 export default App;
